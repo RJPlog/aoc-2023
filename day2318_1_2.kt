6adb82ed5ad1843af449737e3db51e659876b0d1
RJPlog <tfrenz67@gmail.com>
2025-01-12 10:35:50 +0100

    Update day2318_1_2.kt

--- day2318_1_2.kt
+++ day2318_1_2.kt
@@ -4,21 +4,16 @@
 
 import java.io.File
 
-fun lagoon(part: Int = 0): Long {
+fun lagoon(part: Int = 2): Long {
 
-    //File("day2318_puzzle_input.txt").forEachLine {
+    var lagoon = mutableListOf<Pair<Long, Int>>()
 
-    //}
-    
-    var puzzleInput = listOf("R 6 (#70c710)", "D 5 (#0dc571)", "L 2 (#5713f0)", "D 2 (#d2c081)", "R 2 (#59c680)", "D 2 (#411b91)", "L 5 (#8ceee2)", "U 2 (#caa173)", "L 1 (#1b58a2)", "U 2 (#caa171)", "R 2 (#7807d2)", "U 3 (#a77fa3)", "L 2 (#015232)", "U 2 (#7a21e3)")
-
-
-	var lagoon = mutableListOf<Pair<Long, Int>>()
-    
-    puzzleInput.forEach {
-        var dist = it.substring(6, 11).toLong(radix = 16)
-        var dir = it[11].toString().toInt()
-        lagoon.add(Pair(dist, dir))
+    if (part == 2) {
+        File("day2318_puzzle_input.txt").forEachLine {
+            var dist = it.substring(6, 11).toLong(radix = 16)
+            var dir = it[11].toString().toInt()
+            lagoon.add(Pair(dist, dir))          
+        }
     }
     
 
@@ -59,9 +54,6 @@ fun lagoon(part: Int = 0): Long {
     
     return result
 }
-
-   
-
 
 fun main() {
 
