1c63367702e112d5be1a993d0b07ce3889057c83
RJPlog <tfrenz67@gmail.com>
2025-01-29 18:05:28 +0100

    Update day2323_1_2.kt
    tut für das Beispiel, aber nicht mit den original daten

--- day2323_1_2.kt
+++ day2323_1_2.kt
@@ -3,23 +3,70 @@
 
 import java.io.File
 
+fun move(sI: Pair<Int,Int>, dir: Char, eI: Pair<Int,Int>, j: MutableMap<Pair<Int,Int>,Char>, count: Int): Int {
+
+    //if (count == 15) return 0
+    //print("move $sI, $dir:") 
+    //println()
+    var result = mutableListOf(0)
+    if (sI == eI) {
+        result.add(1)
+    } else {
+        // up
+            if (dir != 'd' && j.containsKey(Pair(sI.first, sI.second-1))) {
+                if (j.getValue(Pair(sI.first, sI.second-1)) != 'v') {
+                    result.add(1 + move(Pair(sI.first, sI.second-1), 'u', eI, j, count +1 ))
+                }
+            }
+        // right
+        if (dir != 'l' && j.containsKey(Pair(sI.first+1, sI.second))) {
+            if (j.getValue(Pair(sI.first+1, sI.second)) != '<') {
+                result.add(1 + move(Pair(sI.first+1, sI.second), 'r', eI, j, count +1))
+            }
+        }
+        // down
+        if (dir != 'u' && j.containsKey(Pair(sI.first, sI.second+1))) {
+            if (j.getValue(Pair(sI.first, sI.second+1)) != '^') {
+                result.add(1 + move(Pair(sI.first, sI.second+1), 'd', eI, j, count +1))
+            }
+        }
+        // left
+        if (dir != 'r' && j.containsKey(Pair(sI.first-1, sI.second))) {
+            if (j.getValue(Pair(sI.first-1, sI.second)) != '>') {
+                result.add(1 + move(Pair(sI.first-1, sI.second), 'l', eI, j, count +1))
+            }
+        }
+    }
+    result.sortDescending()
+    //println(result)
+    return result[0]
+}
+
+
+var junctions = mutableMapOf<Pair<Int, Int>, Char>()
+var y = 0
 var w = 0
 var pI = ""
 fun aocDay2323(part: Int = 1): Int {
     File("day2323_puzzle_input.txt").forEachLine {
-        w = it.length
-        pI += it
+            var x = 0
+            pI += it
+            w = it.length
+            it.forEach{
+                if (it!='#') {
+                    junctions.put(Pair(x,y), it)
+                }
+                x+=1
+            }
+            y+=1
     }
-    val h = pI.length / w
-    val startIndex = pI.indexOf(".")
-    val endIndex = pI.lastIndexOf(".")
 
-    // generate List with connections -> skip eventually, could still check neighbor and direction
+    //println(junctions)
+    val startIndex = Pair(pI.indexOf(".") % w, pI.indexOf(".") / w)
+    val endIndex = Pair(pI.lastIndexOf(".") % w, pI.lastIndexOf(".") / w)
 
-    // generate List with junctions
-
-    // set up Dijkstra with longest path
-    return -1
+    val result = move(startIndex, 'd', endIndex, junctions, 1) 
+    return result
 }
 
 
@@ -30,8 +77,7 @@ fun main() {
     println("--- Day 23: A Long Walk ---")
 
     var solution1 = aocDay2323(1)
-    println("   the longest hike is $solution1 steps long")
-
+    println("   the longest hike is $solution1 steps long")  // 2219 is to high
 
     t1 = System.currentTimeMillis() - t1
     println("puzzle solved in ${t1} ms")
