41741dace134a24cb876aac7756bf7e80da7d006
RJPlog <tfrenz67@gmail.com>
2025-01-31 08:42:05 +0100

    Update day2323_1_2.kt
    add step to identify all crossings and distance inbetween (roads) to be used for part 2

--- day2323_1_2.kt
+++ day2323_1_2.kt
@@ -3,6 +3,116 @@
 
 import java.io.File
 
+var roads = mutableMapOf<String, Int>()
+
+fun move(sI: Pair<Int,Int>, dir: Char, eI: Pair<Int,Int>, j: MutableMap<Pair<Int,Int>,Char>, count: Int, startCrossing: Int, laneCount: Int, w: Int): Int {
+    var result = mutableListOf(0)
+    if (sI == eI) {
+        result.add(0)
+        roads.put(startCrossing.toString() + "-" + (sI.first + w * sI.second).toString(), laneCount)
+        println(count)
+    } else {
+        var sC = startCrossing
+        var lC = laneCount
+        var juncCount = 0
+        if (j.containsKey(Pair(sI.first, sI.second-1))) juncCount += 1
+        if (j.containsKey(Pair(sI.first+1, sI.second))) juncCount += 1
+        if (j.containsKey(Pair(sI.first, sI.second+1))) juncCount += 1
+        if (j.containsKey(Pair(sI.first-1, sI.second))) juncCount += 1
+        if (juncCount > 2) {
+            roads.put(sC.toString() + "-" + (sI.first + w * sI.second).toString(), laneCount)
+            sC = sI.first + w * sI.second
+            lC = 0
+        }
+        // up
+        if (dir != 'd' && j.containsKey(Pair(sI.first, sI.second-1))) {
+            if (j.getValue(Pair(sI.first, sI.second-1)) != 'v') {
+                result.add(1 + move(Pair(sI.first, sI.second-1), 'u', eI, j, count +1, sC, lC+1, w))
+            }
+        }
+        // right
+        if (dir != 'l' && j.containsKey(Pair(sI.first+1, sI.second))) {
+            if (j.getValue(Pair(sI.first+1, sI.second)) != '<') {
+                result.add(1 + move(Pair(sI.first+1, sI.second), 'r', eI, j, count +1, sC, lC+1, w))
+            }
+        }
+        // down
+        if (dir != 'u' && j.containsKey(Pair(sI.first, sI.second+1))) {
+            if (j.getValue(Pair(sI.first, sI.second+1)) != '^') {
+                result.add(1 + move(Pair(sI.first, sI.second+1), 'd', eI, j, count +1, sC, lC+1, w))
+            }
+        }
+        // left
+        if (dir != 'r' && j.containsKey(Pair(sI.first-1, sI.second))) {
+            if (j.getValue(Pair(sI.first-1, sI.second)) != '>') {
+                result.add(1 + move(Pair(sI.first-1, sI.second), 'l', eI, j, count +1, sC, lC+1, w))
+            }
+        }
+    }
+    result.sortDescending()
+    return result[0]
+}
+
+
+
+fun aocDay2323(part: Int = 1): Int {
+var junctions = mutableMapOf<Pair<Int, Int>, Char>()
+var y = 0
+var w = 0
+
+var pI = ""
+    File("day2323_puzzle_input.txt").forEachLine {
+            var x = 0
+            pI += it
+            w = it.length
+            it.forEach{
+                if (it!='#') {
+                    if (part == 1) {
+                        junctions.put(Pair(x,y), it)
+                    } else {
+                        junctions.put(Pair(x,y), '.')
+                    }
+                    
+                }
+                x+=1
+            }
+            y+=1
+    }
+
+    val startIndex = Pair(pI.indexOf(".") % w, pI.indexOf(".") / w)
+    val endIndex = Pair(pI.lastIndexOf(".") % w, pI.lastIndexOf(".") / w)
+    println(endIndex)
+
+    var result = 0
+    if (part == 1) {
+        result = move(startIndex, 'd', endIndex, junctions, 1, pI.indexOf("."), 0, w )
+        println("--------roads---------------")
+        println(roads)
+        println("----------------------------")
+    } else {
+        result = move2(startIndex, 'd', endIndex, junctions, 1)
+    } 
+    return result
+}
+
+
+fun main() {
+
+    var t1 = System.currentTimeMillis()
+
+    println("--- Day 23: A Long Walk ---")
+
+    var solution1 = aocDay2323(1)
+    println("   the longest hike is $solution1 steps long")
+
+    var solution2 = aocDay2323(2)
+    println("   the longest hike is $solution2 steps long")
+
+    t1 = System.currentTimeMillis() - t1
+    println("puzzle solved in ${t1} ms")
+}
+
+
 fun move2(sI: Pair<Int,Int>, dir: Char, eI: Pair<Int,Int>, j: MutableMap<Pair<Int,Int>,Char>, count: Int): Int {
     //println(".".repeat(count) + count + sI+ "," + eI)
     var result = mutableListOf(0)
@@ -43,95 +153,3 @@ fun move2(sI: Pair<Int,Int>, dir: Char, eI: Pair<Int,Int>, j: MutableMap<Pair<In
     result.sortDescending()
     return result[0]
 }
-
-fun move(sI: Pair<Int,Int>, dir: Char, eI: Pair<Int,Int>, j: MutableMap<Pair<Int,Int>,Char>, count: Int): Int {
-    var result = mutableListOf(0)
-    if (sI == eI) {
-        result.add(0)
-        println(count)
-    } else {
-        // up
-        if (dir != 'd' && j.containsKey(Pair(sI.first, sI.second-1))) {
-            if (j.getValue(Pair(sI.first, sI.second-1)) != 'v') {
-                result.add(1 + move(Pair(sI.first, sI.second-1), 'u', eI, j, count +1 ))
-            }
-        }
-        // right
-        if (dir != 'l' && j.containsKey(Pair(sI.first+1, sI.second))) {
-            if (j.getValue(Pair(sI.first+1, sI.second)) != '<') {
-                result.add(1 + move(Pair(sI.first+1, sI.second), 'r', eI, j, count +1))
-            }
-        }
-        // down
-        if (dir != 'u' && j.containsKey(Pair(sI.first, sI.second+1))) {
-            if (j.getValue(Pair(sI.first, sI.second+1)) != '^') {
-                result.add(1 + move(Pair(sI.first, sI.second+1), 'd', eI, j, count +1))
-            }
-        }
-        // left
-        if (dir != 'r' && j.containsKey(Pair(sI.first-1, sI.second))) {
-            if (j.getValue(Pair(sI.first-1, sI.second)) != '>') {
-                result.add(1 + move(Pair(sI.first-1, sI.second), 'l', eI, j, count +1))
-            }
-        }
-    }
-    result.sortDescending()
-    return result[0]
-}
-
-
-
-fun aocDay2323(part: Int = 1): Int {
-var junctions = mutableMapOf<Pair<Int, Int>, Char>()
-var y = 0
-var w = 0
-
-var pI = ""
-    File("day2323_puzzle_input.txt").forEachLine {
-            var x = 0
-            pI += it
-            w = it.length
-            it.forEach{
-                if (it!='#') {
-                    if (part == 1) {
-                        junctions.put(Pair(x,y), it)
-                    } else {
-                        junctions.put(Pair(x,y), '.')
-                    }
-                    
-                }
-                x+=1
-            }
-            y+=1
-    }
-
-    val startIndex = Pair(pI.indexOf(".") % w, pI.indexOf(".") / w)
-    val endIndex = Pair(pI.lastIndexOf(".") % w, pI.lastIndexOf(".") / w)
-    println(endIndex)
-
-    var result = 0
-    if (part == 1) {
-        result = move(startIndex, 'd', endIndex, junctions, 1)
-    } else {
-        result = move2(startIndex, 'd', endIndex, junctions, 1)
-    } 
-    return result
-}
-
-
-fun main() {
-
-    var t1 = System.currentTimeMillis()
-
-    println("--- Day 23: A Long Walk ---")
-
-    var solution1 = aocDay2323(1)
-    println("   the longest hike is $solution1 steps long")
-
-    var solution2 = aocDay2323(2)
-    println("   the longest hike is $solution2 steps long")
-
-    t1 = System.currentTimeMillis() - t1
-    println("puzzle solved in ${t1} ms")
-}
-
